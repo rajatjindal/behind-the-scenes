942306b76afe4251b9ed061d79ee2d7070d35719
Rajat Jindal <rajatjindal83@gmail.com>
2023-12-08 22:56:16 +0530

    add runs on cloud label
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- ui/components/TopNavigation.vue
+++ ui/components/TopNavigation.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="bg-primary-500 flex justify-between px-5 h-16">
-    <div class="text-neutral-50 font-bold my-auto flex">
+  <div class="navigation flex justify-between px-5 h-16">
+    <div class="text-neutral-100 font-bold my-auto flex">
       <div class="ml-5">Behind The Scenes</div>
+    </div>
+    <div class="mt-1">
+      <a href="https://www.fermyon.com/cloud"><img src="https://www.fermyon.com/static/image/fermyon-badge.png" /></a>
     </div>
   </div>
 </template>
+<style>
+.navigation {
+  background-color: rgb(111, 111, 223);
+}
+</style>
