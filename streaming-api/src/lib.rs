fe6d838d5fe3f09101649bd629f9e61607535ed2
Rajat Jindal <rajatjindal83@gmail.com>
2024-04-05 22:10:18 +0530

    try compression of image
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- streaming-api/src/lib.rs
+++ streaming-api/src/lib.rs
@@ -1,20 +1,26 @@
 use spin_sdk::http_component;
 
-use futures::StreamExt;
-use anyhow::{Result, anyhow};
+use anyhow::{anyhow, Result};
 use futures::SinkExt;
+use futures::StreamExt;
+use serde::{Deserialize, Serialize};
 use spin_sdk::{
+    http::{
+        self, Headers, IncomingRequest, IncomingResponse, Method, OutgoingRequest,
+        OutgoingResponse, ResponseOutparam, Scheme,
+    },
     key_value::Store,
     variables,
-    http::{self, Headers, IncomingRequest, OutgoingResponse, ResponseOutparam, OutgoingRequest, Method, Scheme, IncomingResponse}
 };
 use url::Url;
-use serde::{Deserialize, Serialize};
+
+use flate2::{write::GzEncoder, Compression};
+use std::io::Write;
 
 /// Send an HTTP request and return the response.
 #[http_component]
 async fn send_outbound(req: IncomingRequest, res: ResponseOutparam) {
-    get_and_stream_imagefile(req, res).await.unwrap();
+    get_and_stream_imagefile(req, res).await.unwrap()
 }
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
@@ -30,11 +36,12 @@ pub struct Post {
 }
 
 async fn get_and_stream_imagefile(req: IncomingRequest, res: ResponseOutparam) -> Result<()> {
-    let (post_id, image_id) = get_post_id_and_image_id_from_url(req.path_with_query().unwrap_or_default());
-    
+    let (post_id, image_id) =
+        get_post_id_and_image_id_from_url(req.path_with_query().unwrap_or_default());
+
     let store = Store::open_default()?;
     let raw_post = store.get(format!("post:{post_id}").as_str())?.unwrap();
-    
+
     let post: Post = serde_json::from_slice(&raw_post)?;
     let url = post.image_map.get(image_id.as_str()).unwrap();
 
@@ -59,24 +66,36 @@ async fn get_and_stream_imagefile(req: IncomingRequest, res: ResponseOutparam) -
 
     let status = response.status();
     if status != 200 {
-        return Err(anyhow!(format!("failed to fetch image from slack. expected 200, got status code {}", status)));
+        return Err(anyhow!(format!(
+            "failed to fetch image from slack. expected 200, got status code {}",
+            status
+        )));
     }
 
     let mut stream = response.take_body_stream();
 
+    let content_type = match response.headers().get("content-type").first() {
+        Some(content_type) => content_type.to_owned(),
+        None => b"image/jpeg".to_vec(),
+    };
+
     let out_response = OutgoingResponse::new(
         status,
-        &Headers::new(&[(
-            "content-type".to_string(),
-            b"application/octet-stream".to_vec(),
-        )]),
+        &Headers::new(&[
+            ("content-type".to_string(), content_type),
+            ("content-encoding".to_string(), b"gzip".to_vec()),
+        ]),
     );
 
     let mut body = out_response.take_body();
     res.set(out_response);
 
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::fast());
     while let Some(chunk) = stream.next().await {
-        body.send(chunk?).await?;
+        // let chunk = chunk?;
+        encoder.write_all(&chunk?).unwrap();
+        body.send(encoder.get_ref().to_vec()).await?;
+        encoder.get_mut().clear()
     }
 
     Ok(())
@@ -88,5 +107,5 @@ fn get_post_id_and_image_id_from_url(path_and_query: String) -> (String, String)
     let post_id = parts[0];
     let image_id = parts[2];
 
-    return (post_id.to_string(), image_id.to_string())
+    return (post_id.to_string(), image_id.to_string());
 }
